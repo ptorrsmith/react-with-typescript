99eb3921fb2a7f93b7481f20c3c34c7a499703ae
Peter Torr Smith <peter.torr.smith@cultureamp.com>
2021-11-19 09:43:07 +1300

    introduce useState for local component state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [number, setNumber] = useState<number>(5); // don't need to define as inferred
+
+  const changeNumber = () => {
+    setNumber(10);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
