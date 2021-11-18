f624cdb752dd5323f41bd63913011df2b357445a
Peter Torr Smith <peter.torr.smith@cultureamp.com>
2021-11-19 11:11:31 +1300

    add renderList function and use within our <ul />

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -11,13 +11,29 @@ interface iProps {
 
 const List: React.FC<iProps> = ({ people }) => {
 
+  const renderList = ():JSX.Element[] => { // define the type of the return value for this function
+    return people.map((person) => {
+      return (
+        <li className="List">
+        <div className="List-header">
+          <img src={person.url} alt={person.name} className="List-img" />
+          <h2>{person.name}</h2>
+        </div>
+        <p>{person.age} years old</p>
+        <p className="List-note">{person.note}</p>
+      </li>
+      )
+    })
+  }
+
   return (
     <ul>
-      {people.map(person => {
+      {renderList()}
+      {/* {people.map(person => {
         return (
           <div key={person.name}>{person.name}, {person.age}</div>
         )
-      })}
+      })} */}
     </ul>
   )
 }
